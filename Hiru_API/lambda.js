f04774e37cb9c4a3ab51dbb56d3c132dd3fd3fad
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-05 18:24:03 +0530

    initial commit

---
+++ Hiru_API/lambda.js
@@ -0,0 +1,44 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+
+
+	ddb.put({
+		TableName: 'Hiru',
+		Item: {}
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
+
+
+
+	callback(null, 'Successfully executed');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
